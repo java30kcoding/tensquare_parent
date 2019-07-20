721ec8ea724a7bf88f0bd13aa477729db5232542
yuanyulou <434356430@qq.com>
2019-07-20 21:39:57 +0800

    添加短信模块

---
+++ tensquare_sms/src/main/java/com/tensquare/sms/listener/SmsListener.java
@@ -0,0 +1,34 @@
+package com.tensquare.sms.listener;
+
+import com.aliyuncs.exceptions.ClientException;
+import com.tensquare.sms.utils.SmsUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+@Component
+@RabbitListener(queues = "sms")
+@Slf4j
+public class SmsListener {
+
+    @Resource
+    private SmsUtil smsUtil;
+
+    @RabbitHandler
+    public void executeSms(Map<String, String> map) {
+        String mobile = map.get("mobile");
+        String checkCode = map.get("check_code");
+        log.info("手机号： {}" + mobile);
+        log.info("验证码： {}" + checkCode);
+        try {
+            smsUtil.sendSms(mobile, "SMS_158440564", "金鑫自己测试学习用的签名", "{\"code\": \"" + checkCode + "\"}");
+        } catch (ClientException e) {
+            e.printStackTrace();
+        }
+    }
+}
